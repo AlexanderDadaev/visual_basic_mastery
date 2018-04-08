195cd6981e6d912ea628410e01f0a386d92005df
Alexander Dadaev <37238432+AlexanderDadaev@users.noreply.github.com>
2018-04-08 19:56:36 +0300

    Add files via upload

---
+++ excel_file_merge/excel_file_merge.vb
@@ -0,0 +1,55 @@
+Sub MergeFiles()
+     
+    Dim Path            As String
+    Dim FileName        As String
+    Dim Wkb             As Workbook
+    Dim WS              As Worksheet
+     
+    Application.EnableEvents = False
+    Application.ScreenUpdating = False
+    Path = ThisWorkbook.Path & "\ñåíòÿáðü 2016" 'file path subject to change
+    FileName = Dir(Path & "\*.xls", vbNormal)
+    Do Until FileName = ""
+        Set Wkb = Workbooks.Open(FileName:=Path & "\" & FileName, Password:="100")
+        For Each WS In Wkb.Worksheets
+            WS.Unprotect Password:="139"
+            
+            WS.AutoFilterMode = False
+            
+            'On Error Resume Next
+            'WS.ShowAllData
+            'On Error GoTo 0
+            
+            WS.Copy After:=ThisWorkbook.Sheets(ThisWorkbook.Sheets.Count)
+        Next WS
+        Wkb.Close False
+        FileName = Dir()
+    Loop
+    
+    Dim J As Integer
+
+    On Error Resume Next
+    Sheets(1).Select
+    Worksheets.Add
+    Sheets(1).Name = "ComboMerge"
+    
+    Sheets(3).Activate
+    Range("A1").EntireRow.Select
+    Selection.Copy Destination:=Sheets(1).Range("A1")
+
+    For J = 3 To Sheets.Count
+        Sheets(J).Activate
+        Range("A1").Select
+        Selection.CurrentRegion.Select
+        Selection.Offset(1, 0).Resize(Selection.Rows.Count - 1).Select
+        Selection.Copy Destination:=Sheets(1).Range("A65536").End(xlUp)(2)
+    Next
+    
+    For Each WS In Sheets
+    Application.DisplayAlerts = False
+    If WS.Name <> "ComboMerge" Then WS.Delete
+    Next
+    Application.DisplayAlerts = True
+     
+End Sub
+
